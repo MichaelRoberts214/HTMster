f4550b6a544345087499c11e881c76568f765e02
Michael Roberts <m_roberts6@u.pacific.edu>
2015-05-15 17:21:45 -0700

    add form validation in script.js

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ context.fill();
 context.lineWidth = 2;
 context.strokeStyle = '#FFFFFF';
 context.stroke();
+
+var validateForm = function() {
+  var firstInput = document.forms["myForm"]["first"].value;
+  var secondInput = document.forms["myForm"]["second"].value;
+  var thirdInput = document.forms["myForm"]["third"].value;
+
+  if(firstInput === null || firstInput === "") {
+    alert('First Input Required');
+    return false;
+  }
+
+  else if(secondInput === null || secondInput === "") {
+    alert('Second Input Required');
+    return false;
+  }
+
+  else if(thirdInput === null || thirdInput === "") {
+    alert('Third Input Required');
+    return false;
+  }
+}
